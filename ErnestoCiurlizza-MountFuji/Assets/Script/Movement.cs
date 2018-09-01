eea4ed402fe2a481984a226adfeebf8a407b4882
tatociurlizza12 <tciurlizzas@gmail.com>
2018-09-01 16:26:54 -0500

    Parcial 1

---
+++ ErnestoCiurlizza-MountFuji/Assets/Script/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(0, 0, speed);
+	}
+}
---
+++ ErnestoCiurlizza-MountFuji/Assets/Texture/747-400/readme.txt
@@ -0,0 +1,4 @@
+747-400
+
+format ; 3ds,abc,blend,dae,fbx,mtl,Obj,x3d
+enjoy
